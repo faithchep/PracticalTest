7796385f6cd4bfd26b2c3f29684c95da857111cf
faithchep <faithchepkoech40@gmail.com>
2022-01-20 09:25:30 +0300

    front-end

--- app/src/App.js
+++ app/src/App.js
@@ -1,15 +1,22 @@
-import React from "react";
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
-import clientsComponent from "./components/clientsComponent";
+import Home from './Home';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import ClientList from './ClientList';
+import ClientEdit from "./ClientEdit";
 
-function App() {
-  return (
-    <div className="App">
-      <clientsComponent />
-
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <Router>
+          <Switch>
+            <Route path='/' exact={true} component={Home}/>
+            <Route path='/clients' exact={true} component={ClientList}/>
+            <Route path='/clients/:id' component={ClientEdit}/>
+          </Switch>
+        </Router>
+    )
+  }
 }
 
 export default App;
--- src/main/java/com/faith/restapi/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.faith.restapi.repository;
-
-import com.faith.restapi.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer>{
-
-}
